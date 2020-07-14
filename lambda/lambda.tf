53116e9ed8d498c0dfdae5346605312c262efde3
madhuguptalive <madhu.gupta@bkash.com>
2020-07-14 13:11:35 +0600

    Update lambda.tf

--- lambda/lambda.tf
+++ lambda/lambda.tf
@@ -9,10 +9,10 @@ resource "aws_lambda_function" "ec2Refresh" {
   filename = data.archive_file.ec2Refresh.output_path
   function_name = "ec2Refresh-tf"
   role = aws_iam_role.ec2Refresh.arn
-  handler          = "index.handler"
-  runtime          = "nodejs10.x"
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "Python 3.8"
   timeout          = "90"
-  memory_size      = "512"
+  memory_size      = "128"
 #  source_code_hash = filebase64(file("${data.archive_file.ec2Refresh.output_path}"))
   publish = false
   environment {
